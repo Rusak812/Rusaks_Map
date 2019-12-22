49fe87bb1aee73c66b8d1ca81eb20ddc8ebc48ef
Rusak812 <58809701+Rusak812@users.noreply.github.com>
2019-12-23 01:15:45 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setup(
+    name='Demographics_dynamics',
+    version='0.0.1',
+    author='Akmetdinov Ruslan',
+    author_email='rusak812@gmail.com',
+    description='Demographics dynamics',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://phys.msu.ru',
+    license='SCR',
+    packages=find_packages(),
+    # If all your code are in a module, use py_modules instead of packages:
+    # py_modules=['ser'],
+    # scripts=['bin/serpens'],
+    test_suite='test',
+    install_requires=['numpy>=1.13', 'matplotlib>=2.0', 'scipy=>1.3'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: SCR License',
+        'Topic :: Education',
+        'Programming Language :: Python :: 3',
+        # See full list on https://pypi.org/classifiers/
+    ],
+    keywords='sample science astrophysics',
+)
