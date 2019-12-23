383e165bb3290e8e0eebf11668d3502b63c058c0
Rusak812 <58809701+Rusak812@users.noreply.github.com>
2019-12-23 16:23:06 +0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,21 +8,12 @@ pop = data_Foundation['Population']
 class MyTestCase(unittest.TestCase):
 
     def test_Pareto(self):
-        if (Pareto(pop) > 0):
-            result = True
-        else:
-            result = False
-        self.assertTrue(result, " Ошибка в сортировки данных ")
+        self.assertTrue(Pareto(pop) > 0, " Ошибка в сортировки данных ")
 
     def test_Integ(self):
         temp = (average_population(pop) - np.trapz(pop))/average_population(pop)
-        print(temp)
         sigma = 1.3
-        if (temp < sigma):
-            result = True
-        else:
-            result = False
-        self.assertTrue(result, " Плохой анализ интегралов ")
+        self.assertTrue(temp < sigma, " Плохой анализ интегралов ")
 
 if __name__ == '__main__':
     unittest.main()
