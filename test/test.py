a4438d1be8df551469c2402c7585f3061d7aa4c1
Rusak812 <58809701+Rusak812@users.noreply.github.com>
2019-12-23 14:18:21 +0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,13 +2,13 @@ import unittest
 import numpy as np
 
 from my_functions import average_population, Pareto
+data_Foundation = np.genfromtxt("Foundation.data",names=True)
+pop = data_Foundation['Population']
 
 class MyTestCase(unittest.TestCase):
-    data_Foundation = np.genfromtxt('population_data/'+"Foundation.data",names=True)
-    pop = data_Foundation['Population']
-    
+
     def test_Pareto(self):
-        if (Pareto(pop) < 0):
+        if (Pareto(pop) > 0):
             result = True
         else:
             result = False
@@ -16,8 +16,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_Integ(self):
         temp = (average_population(pop) - np.trapz(pop))/average_population(pop)
-        sigma = 0.3
-        if (temp > sigma):
+        print(temp)
+        sigma = 1.3
+        if (temp < sigma):
             result = True
         else:
             result = False
